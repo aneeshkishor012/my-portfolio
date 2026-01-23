6a8705c6e6c7442931e08d40c30380b50ba6cb71
Aneesh Kishor K <114078154+aneeshLinkEZ@users.noreply.github.com>
2026-01-23 19:51:13 +0530

    sync changes

--- next.config.ts
+++ next.config.ts
@@ -4,21 +4,21 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export", // 👈 Required for static export
-  trailingSlash: true, // 👈 ensures relative path routing works on GitHub Pages
-  images: {
-    unoptimized: true, // 👈 Avoid using Next.js Image optimization (not supported in static export)
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.pinimg.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
-  basePath: isProd ? "/my-portfolio" : "", // 👈 your repo name here
-  assetPrefix: isProd ? "/my-portfolio/" : "",
+  // output: "export", // 👈 Required for static export
+  // trailingSlash: true, // 👈 ensures relative path routing works on GitHub Pages
+  // images: {
+  //   unoptimized: true, // 👈 Avoid using Next.js Image optimization (not supported in static export)
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "i.pinimg.com",
+  //       port: "",
+  //       pathname: "/**",
+  //     },
+  //   ],
+  // },
+  // basePath: isProd ? "/my-portfolio" : "", // 👈 your repo name here
+  // assetPrefix: isProd ? "/my-portfolio/" : "",
 };
 
 export default nextConfig;
